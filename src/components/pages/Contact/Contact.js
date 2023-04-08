bc665074694f92d7db6c1db8b606296d31c938a8
Andrew Chou <andrewchou1996@gmail.com>
2023-04-08 16:53:50 -0700

    Finish contact page

--- src/components/pages/Contact/Contact.js
+++ src/components/pages/Contact/Contact.js
@@ -1,25 +1,118 @@
-import React from "react";
+import React, { useState } from "react";
+import "./contact.css";
 
 export default function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [nameError, setNameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!name || !email || !message) {
+      alert("Please fill in all required fields");
+      return;
+    }
+
+    if (!isValidEmail(email)) {
+      alert("Please enter a valid email address");
+      return;
+    }
+
+    // submit form
+  };
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleMessageChange = (event) => {
+    setMessage(event.target.value);
+  };
+
+  const handleNameBlur = () => {
+    if (!name) {
+      setNameError(true);
+    } else {
+      setNameError(false);
+    }
+  };
+
+  const handleEmailBlur = () => {
+    if (!email) {
+      setEmailError(true);
+    } else {
+      setEmailError(false);
+    }
+  };
+
   return (
     <section id="contact">
-      <div>
-        <h1>Contact Page</h1>
-        <p>
-          Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem,
-          quis molestie urna. Aliquam semper ultrices varius. Aliquam faucibus
-          sit amet magna a ultrices. Aenean pellentesque placerat lacus
-          imperdiet efficitur. In felis nisl, luctus non ante euismod, tincidunt
-          bibendum mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus
-          quis magna posuere, eget tristique dui dapibus. Maecenas fermentum
-          elementum faucibus. Quisque nec metus vestibulum, egestas massa eu,
-          sollicitudin ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt
-          nunc in nibh dictum ullamcorper. Class aptent taciti sociosqu ad
-          litora torquent per conubia nostra, per inceptos himenaeos. Etiam
-          ornare rutrum felis at rhoncus. Etiam vel condimentum magna, quis
-          tempor nulla.
-        </p>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name <span className="text-danger">*</span>
+          </label>
+          <input
+            type="text"
+            className={`form-control ${nameError ? "is-invalid" : ""}`}
+            id="name"
+            value={name}
+            onChange={handleNameChange}
+            onBlur={handleNameBlur}
+            required
+          />
+          {nameError && (
+            <div className="invalid-feedback">Please enter your name</div>
+          )}
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email Address <span className="text-danger">*</span>
+          </label>
+          <input
+            type="email"
+            className={`form-control ${emailError ? "is-invalid" : ""}`}
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+            onBlur={handleEmailBlur}
+            required
+          />
+          {emailError && (
+            <div className="invalid-feedback">
+              Please enter a valid email address
+            </div>
+          )}
+        </div>
+        <div className="mb-3">
+          <label htmlFor="message" className="form-label">
+            Message <span className="text-danger">*</span>
+          </label>
+          <textarea
+            className="form-control"
+            id="message"
+            rows="5"
+            value={message}
+            onChange={handleMessageChange}
+            required
+          ></textarea>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
     </section>
   );
+}
+
+function isValidEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
 }
